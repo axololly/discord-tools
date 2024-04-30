b56f420423126061cd36b1b128b081b12d0b4776
axololly <161346834+axololly@users.noreply.github.com>
2024-04-30 11:11:41 +0100

    Update connect4_check_win.py

--- connect4_check_win.py
+++ connect4_check_win.py
@@ -1,6 +1,7 @@
 class Connect4_CheckWins:
     ...
-    def is_game_over(self):
+    def check_win(self):
+        # rotate the board for checking columns easier
         rotated_board = [
             [
                 self.board[-1 - i][x] for i, _ in enumerate(self.board)
@@ -8,20 +9,25 @@ class Connect4_CheckWins:
             for x, _ in enumerate(self.board[0])
         ]
 
+        # vertical and horizontal
         for board in [self.board, rotated_board]:
             for row in board:
                 for i in range(4):
-                    if all(row[i:i + 4]) in (1, 2):
-                        return all(row[i:i + 4])
+                    line = row[i : i + 4]
+                    
+                    if len(set(line)) == 1 and 0 not in line:
+                        return line[0]
 
         # diagonals
         for x_change in (1, -1):
             for i in range(3):
                 for j in range(4):
-                    if all([self.board[i + x][j + x * x_change] for x in range(4)]) in (1, 2):
-                        return all([self.board[i + x][j + x * x_change] for x in range(4)])  
+                    line = [self.board[i + x][j + x * x_change] for x in range(4)]
                     
-        if self.board[0].count(0) == 0:
+                    if len(set(line)) == 1 and 0 not in line:
+                        return line[0]
+                    
+        if not self.valid_moves():
             return 0
         
         return False
